67440cd74f81caa96e7486034b5734b93e8271d4
Daehee Yoon <allendy11@gmail.com>
2022-06-06 23:23:14 +0900

    [server] add extractJWT, add interface IUser, update token in server object

--- server/src/config/config.ts
+++ server/src/config/config.ts
@@ -5,9 +5,18 @@ dotenv.config();
 // server
 const SERVER_HOST = process.env.SERVER_HOST || "localhost";
 const SERVER_PORT = process.env.SERVER_PORT || "4000";
+const SERVER_TOKEN_ISSUER = process.env.SERVER_TOKEN_ISSUER || "tobid";
+const SERVER_TOKEN_SECRET = process.env.SERVER_TOKEN_SECRET || "tobidsecret";
+const SERVER_TOKEN_EXPIRETIME = process.env.SERVER_TOKEN_EXPIRETIME || "10m";
+
 const server = {
   host: SERVER_HOST,
   port: SERVER_PORT,
+  token: {
+    issuer: SERVER_TOKEN_ISSUER,
+    secret: SERVER_TOKEN_SECRET,
+    expireTime: SERVER_TOKEN_EXPIRETIME,
+  },
 };
 
 // mysql
