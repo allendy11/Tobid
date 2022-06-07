c428ed54292a03807f8c1ccd2fc40e3a69f57850
Daehee Yoon <allendy11@gmail.com>
2022-06-07 22:08:40 +0900

    [hotfix] change name extractJWT to verifyJWT

--- server/src/routes/user.ts
+++ server/src/routes/user.ts
@@ -1,12 +1,12 @@
 import express, { Request, Response, NextFunction } from "express";
 import test from "../contollers/test";
 import controller from "../contollers/user";
-import extractJWT from "../middleware/extractJWT";
+import verifyJWT from "../middleware/verifyJWT";
 
 const router = express.Router();
 
 router.post("/test", test);
-router.get("/validate", extractJWT, controller.validateToken);
+router.get("/validate", verifyJWT, controller.validateToken);
 router.post("/register", controller.register);
 router.post("/login", controller.login);
 export default router;
