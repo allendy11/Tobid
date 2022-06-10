a0df0a66feaf223f4fa2e05cf19aaa4eff846fd7
Daehee Yoon <allendy11@gmail.com>
2022-06-10 14:04:01 +0900

    [feature] add getPost

--- server/src/contollers/post.ts
+++ server/src/contollers/post.ts
@@ -35,7 +35,38 @@ const allPosts = (req: Request, res: Response, next: NextFunction) => {
       });
     });
 };
-const getPost = (req: Request, res: Response, next: NextFunction) => {};
+const getPost = (req: Request, res: Response, next: NextFunction) => {
+  Connect()
+    .then((connection: any) => {
+      const query = `SELECT * FROM posts INNER JOIN users ON posts.user_id = users.id WHERE (users.id = ? AND posts.id = ?`;
+      const params = [res.locals.jwt.id, req.params.id];
+      Query(connection, query, params)
+        .then((result: any) => {
+          logging.info(
+            NAMESPACE,
+            `[getPost-success] [userId:${res.locals.jwt.id}] [postId: ${req.params.id}]`
+          );
+          res.status(200).json({
+            message: `Get post id:${req.params.id}`,
+            post: result[0],
+          });
+        })
+        .catch((error) => {
+          logging.error(NAMESPACE, `[getPost-Query] ${error.message}`);
+          res.status(500).json({
+            message: error.message,
+            error,
+          });
+        });
+    })
+    .catch((error) => {
+      logging.error(NAMESPACE, `[getPost-Connect] ${error.message}`);
+      res.status(500).json({
+        message: error.message,
+        error,
+      });
+    });
+};
 const writePost = (req: Request, res: Response, next: NextFunction) => {};
 const updatePost = (req: Request, res: Response, next: NextFunction) => {};
 const deletePost = (req: Request, res: Response, next: NextFunction) => {};
