4b3a027c4e38dbcb8a800d5100a029ae3606b76c
Daehee Yoon <allendy11@gmail.com>
2022-06-10 13:01:07 +0900

    [hotfix] fix api

--- server/src/routes/board.ts
+++ server/src/routes/board.ts
@@ -3,10 +3,10 @@ import controller from "../contollers/board";
 import verifyJWT from "../middleware/verifyJWT";
 const router = express.Router();
 
-router.get("/board/all", verifyJWT, controller.getBoard);
-router.get("/board/:id", verifyJWT, controller.getBoard);
-router.post("/board", verifyJWT, controller.writeBoard);
-router.put("/board/:id", verifyJWT, controller.updateBoard);
-router.delete("/board/:id", verifyJWT, controller.deleteBoard);
+router.get("/all", verifyJWT, controller.allBoard);
+router.get("/:id", verifyJWT, controller.getBoard);
+router.post("/", verifyJWT, controller.writeBoard);
+router.put("/:id", verifyJWT, controller.updateBoard);
+router.delete("/:id", verifyJWT, controller.deleteBoard);
 
 export default router;
