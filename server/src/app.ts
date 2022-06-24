d2c1ce6e2b3ee0a546993577d13ffea72bd5a3d4
Daehee Yoon <allendy11@gmail.com>
2022-06-25 00:01:50 +0900

    [deploy] test v1.2.0 + fix cors

--- server/src/app.ts
+++ server/src/app.ts
@@ -15,7 +15,13 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
-app.use(cors());
+app.use(
+  cors({
+    origin: ["*"],
+    methods: ["OPTIONS", "GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+  })
+);
 
 // log
 app.use((req: Request, res: Response, next: NextFunction) => {
