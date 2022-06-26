4812b8d2cc6a83ec765128d56ee094a50b7e3bff
Daehee Yoon <allendy11@gmail.com>
2022-06-26 14:55:10 +0900

    [feature] add nav

--- client/src/components/Nav.tsx
+++ client/src/components/Nav.tsx
@@ -1,7 +1,22 @@
 import React from "react";
-
+import "./css/Nav.css";
+import { Link } from "react-router-dom";
 const Nav = () => {
-  return <div>Nav</div>;
+  return (
+    <div className="nav-container">
+      <Link to="/">
+        <div className="nav-box nav-title">
+          <img src={process.env.PUBLIC_URL + "logo_32.png"} alt="" />
+          <span>Tobid</span>
+        </div>
+      </Link>
+      <div className="nav-box nav-user">
+        <Link to="/login">로그인</Link>
+        <span className="bar"></span>
+        <Link to="/signup">회원가입</Link>
+      </div>
+    </div>
+  );
 };
 
 export default Nav;
