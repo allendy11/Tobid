b32d2e7fc66f753db05a182193df3b9891687c37
Daehee Yoon <allendy11@gmail.com>
2022-06-26 11:48:05 +0900

    [deploy] test v1.2.5 + fix server port

--- server/src/config/config.ts
+++ server/src/config/config.ts
@@ -4,7 +4,7 @@ dotenv.config();
 
 // server
 const SERVER_HOST = process.env.SERVER_HOST || "localhost";
-const SERVER_PORT = process.env.SERVER_PORT || "4000";
+const SERVER_PORT = process.env.SERVER_PORT || "8080";
 const SERVER_TOKEN_ISSUER = process.env.SERVER_TOKEN_ISSUER || "tobid";
 const SERVER_TOKEN_SECRET = process.env.SERVER_TOKEN_SECRET || "tobidsecret";
 const SERVER_TOKEN_EXPIRETIME = process.env.SERVER_TOKEN_EXPIRETIME || "10m";
