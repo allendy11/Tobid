d29c853d22013ac4fd7f921e2b9be2260aa40843
Daehee Yoon <allendy11@gmail.com>
2022-06-27 23:40:08 +0900

    [client] update landing : add contentBox

--- client/src/pages/Landing.tsx
+++ client/src/pages/Landing.tsx
@@ -4,9 +4,13 @@ const Landing = () => {
   return (
     <div id="Landing">
       <div className="container">
-        <div className="box"></div>
-        <div className="box"></div>
-        <div className="box"></div>
+        <div className="box">
+          <div>Your goods</div>
+          <div>Join TOBID</div>
+        </div>
+        <div className="box">
+          <img src="#" alt="Image" />
+        </div>
       </div>
     </div>
   );
