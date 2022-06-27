3c5bb96f987e2857c586d69e9d6b97d82b84bc26
Daehee Yoon <allendy11@gmail.com>
2022-06-27 22:51:30 +0900

    [client] add landing

--- client/src/pages/Landing.tsx
+++ client/src/pages/Landing.tsx
@@ -1,7 +1,15 @@
 import React from "react";
-
+import "./css/Landing.css";
 const Landing = () => {
-  return <div>Landing</div>;
+  return (
+    <div id="Landing">
+      <div className="container">
+        <div className="box"></div>
+        <div className="box"></div>
+        <div className="box"></div>
+      </div>
+    </div>
+  );
 };
 
 export default Landing;
