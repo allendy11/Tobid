00e29db83a0786fc1bc62a403ef804c81c3aa5a2
Daehee Yoon <allendy11@gmail.com>
2022-06-29 00:11:27 +0900

    [client] fix login modal: delete unnecessary stuff

--- client/src/components/Login/LoginModal.tsx
+++ client/src/components/Login/LoginModal.tsx
@@ -1,31 +1,27 @@
 import React from "react";
 import "./LoginModal.css";
+import { Link } from "react-router-dom";
 const LoginModal = () => {
   return (
     <div id="LoginModal">
-      <div className="container">
-        <div className="box">
+      <div className="modal-container">
+        <div className="modal-box">
           <div>
             <input type="email" placeholder="Email" />
           </div>
           <div>
             <input type="password" placeholder="Password" />
           </div>
-          <div>
-            <input type="checkbox" />
-            <span>Remember Email</span>
-          </div>
+          <div>err msg: something wrong</div>
           <div>Login</div>
           <div>
-            <span>Singup</span>
-            <span className="bar">|</span>
-            <span>Find Email</span>
-            <span className="bar">|</span>
-
-            <span>Find Password</span>
+            <span>Don't have an account?</span>
+            <Link to="/register">
+              <span>Register</span>
+            </Link>
           </div>
         </div>
-        <div className="box"></div>
+        <div className="modal-box"></div>
       </div>
     </div>
   );
