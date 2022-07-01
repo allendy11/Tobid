a58027142ca0d850e6f6b9d235bd9835fcfb308a
Daehee Yoon <allendy11@gmail.com>
2022-07-01 22:06:38 +0900

    [client] update register : add changeHandler, clickHandler

--- client/src/components/Register/RegisterModal.tsx
+++ client/src/components/Register/RegisterModal.tsx
@@ -1,27 +1,106 @@
-import React from "react";
+import React, { useState } from "react";
 import "./RegisterModal.css";
 import { Link } from "react-router-dom";
+import axios from "axios";
+import dotenv from "dotenv";
+dotenv.config();
 const RegisterModal = () => {
+  const [userInput, setUserInput] = useState({
+    username: "",
+    email: "",
+    password: "",
+    password2: "",
+  });
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    if (e.target && e.target.id === "register-username") {
+      setUserInput({
+        ...userInput,
+        username: e.target.value,
+      });
+    } else if (e.target && e.target.id === "register-email") {
+      setUserInput({
+        ...userInput,
+        email: e.target.value,
+      });
+    } else if (e.target && e.target.id === "register-password") {
+      setUserInput({
+        ...userInput,
+        password: e.target.value,
+      });
+    } else if (e.target && e.target.id === "register-password2") {
+      setUserInput({
+        ...userInput,
+        password2: e.target.value,
+      });
+    }
+  };
+  const handleClick = (e: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
+    axios({
+      method: "POST",
+      url: `${process.env.REACT_APP_SERVER_URL_LOCAL}/user/register`,
+      data: {
+        username: userInput.username,
+        email: userInput.email,
+        password: userInput.password,
+      },
+    }).then((res) => {
+      console.log(res.data);
+    });
+  };
   return (
     <div id="RegisterModal">
       <div className="modal-container">
         <div className="modal-box">
           <div>
-            <input type="text" placeholder="Username" />
+            <input
+              id="register-username"
+              type="text"
+              placeholder="Username"
+              onChange={(e) => {
+                handleChange(e);
+              }}
+            />
           </div>
           <div>
-            <input type="text" placeholder="Email" />
+            <input
+              id="register-email"
+              type="text"
+              placeholder="Email"
+              onChange={(e) => {
+                handleChange(e);
+              }}
+            />
           </div>
           <div>
-            <input type="password" placeholder="Password" />
+            <input
+              id="register-password"
+              type="password"
+              placeholder="Password"
+              onChange={(e) => {
+                handleChange(e);
+              }}
+            />
           </div>
           <div>
-            <input type="password" placeholder="Re-type Password" />
+            <input
+              id="register-password2"
+              type="password"
+              placeholder="Re-type Password"
+              onChange={(e) => {
+                handleChange(e);
+              }}
+            />
           </div>
           <div>error msg : something wrong</div>
         </div>
         <div className="modal-box">
-          <div>Complete</div>
+          <div
+            onClick={(e) => {
+              handleClick(e);
+            }}
+          >
+            Complete
+          </div>
           <div>
             <span>Already have an account?</span>
             <Link to="/login">
