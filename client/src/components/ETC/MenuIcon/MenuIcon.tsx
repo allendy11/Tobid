577cc151539814e08c340eb75850679eb8bee0c9
Daehee Yoon <allendy11@gmail.com>
2022-07-07 00:01:36 +0900

    [client] fix nav: change design

---
+++ client/src/components/ETC/MenuIcon/MenuIcon.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./MenuIcon.css";
+const MenuIcon = () => {
+  return (
+    <div id="MenuIcon">
+      <span className="bar"></span>
+      <span className="bar"></span>
+      <span className="bar"></span>
+    </div>
+  );
+};
+
+export default MenuIcon;
