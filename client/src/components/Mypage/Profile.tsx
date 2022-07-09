8b9bdd036c13f7e9c8663fb409d558a1c6b5fc00
Daehee Yoon <allendy11@gmail.com>
2022-07-10 00:58:23 +0900

    [client] update mypage: add image handler

--- client/src/components/Mypage/Profile.tsx
+++ client/src/components/Mypage/Profile.tsx
@@ -1,10 +1,30 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 import "./Profile.css";
 import dotenv from "dotenv";
 dotenv.config();
 const Profile = () => {
-  const imageHandler = (e: React.ChangeEvent<HTMLInputElement>) => {};
-
+  const [userInfo, setUserInfo] = useState({
+    username: "",
+    email: "",
+  });
+  const [userImage, setUserImage] = useState({
+    default: false,
+    url: `${process.env.PUBLIC_URL}/images/default/my_image.png`,
+  });
+  const imageChange = (e: React.ChangeEvent<HTMLInputElement>) => {};
+  const imageClick = (e: React.MouseEvent<HTMLImageElement, MouseEvent>) => {
+    if (userImage.default) {
+      inputRef.current && inputRef.current.click();
+    } else {
+    }
+  };
+  const handleClick = (e: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
+    setUserImage({
+      default: true,
+      url: `${process.env.PUBLIC_URL}/images/default/my_image.png`,
+    });
+  };
+  const inputRef = useRef<HTMLInputElement>(null);
   return (
     <div id="Profile">
       <div className="profile-container">
@@ -18,15 +38,22 @@ const Profile = () => {
               type="file"
               accept="image/*"
               style={{ display: "none" }}
-              onChange={(e) => imageHandler(e)}
+              ref={inputRef}
+              onChange={(e) => imageChange(e)}
             />
-            <label htmlFor="add-image">
-              <img
-                id="my-image"
-                src={`${process.env.PUBLIC_URL}/images/default/my_image.png`}
-                alt="My Image"
-              />
-            </label>
+            <img
+              id="my-image"
+              src={userImage.url}
+              alt="My Image"
+              onClick={(e) => imageClick(e)}
+            />
+            <div className="delete-button">
+              {userImage.default ? (
+                ""
+              ) : (
+                <div onClick={(e) => handleClick(e)}>Delete</div>
+              )}
+            </div>
           </form>
         </div>
         <div className="userInfo-box"></div>
