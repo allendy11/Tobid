7d9e6de84f66e9da7dc2f7fa4a6baa51ee09ae0e
Daehee Yoon <allendy11@gmail.com>
2022-07-14 18:56:33 +0900

    [client] add editUserImage

--- client/src/components/Mypage/UserImage.tsx
+++ client/src/components/Mypage/UserImage.tsx
@@ -1,22 +1,54 @@
 import React, { useRef } from "react";
 import IUser from "../../Interface/IUser";
-const UserImage = ({ profileImage }: { profileImage: string }) => {
+import axios from "axios";
+import dotenv from "dotenv";
+dotenv.config();
+const UserImage = ({
+  userInfo,
+  setUserInfo,
+}: {
+  userInfo: IUser["userInfo"];
+  setUserInfo: IUser["setUserInfo"];
+}) => {
   const inputRef = useRef<HTMLInputElement>(null);
-  const imageChange = (e: React.ChangeEvent<HTMLInputElement>) => {};
+
+  const imageChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const reader = new FileReader();
+    reader.onload = () => {
+      if (reader.result) {
+        setUserInfo({
+          ...userInfo,
+          image: String(reader.result),
+        });
+      }
+    };
+    if (e.target.files) {
+      reader.readAsDataURL(e.target.files[0]);
+      const formData = new FormData();
+      formData.append("img", e.target.files[0]);
+      const token_local = localStorage.getItem("token_local");
+      if (token_local) {
+        const _token_local = JSON.parse(token_local);
+        axios({
+          method: "PUT",
+          url: `${process.env.REACT_APP_SERVER_URL_LOCAL}/user/${userInfo.id}/image`,
+          data: formData,
+          headers: {
+            authorization: `Bearer ${_token_local}`,
+            "content-type": "multipart/form-data",
+          },
+        }).then((res) => console.log(res.data));
+      }
+    }
+  };
   const imageClick = (e: React.MouseEvent<HTMLImageElement, MouseEvent>) => {
-    // if (image.default) {
-    //   inputRef.current && inputRef.current.click();
-    // }
+    inputRef.current && inputRef.current.click();
   };
-  const handleClick = (e: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
-    // setUserImage({
-    //   default: true,
-    //   url: `${process.env.PUBLIC_URL}/images/default/my_image.png`,
-    // });
-  };
+  const handleClick = (e: React.MouseEvent<HTMLDivElement, MouseEvent>) => {};
+  // console.log(userInfo);
   return (
     <div id="UserImage">
-      <form method="post" encType="multipart/form-data">
+      <form method="put" encType="multipart/form-data">
         <input
           id="add-image"
           type="file"
@@ -28,15 +60,15 @@ const UserImage = ({ profileImage }: { profileImage: string }) => {
         <img
           id="profile-image"
           src={
-            profileImage === null
+            userInfo.image === null
               ? `${process.env.PUBLIC_URL}/images/default/my_image.png`
-              : profileImage
+              : userInfo.image
           }
           alt="Profile Image"
           onClick={(e) => imageClick(e)}
         />
         <div className="delete-button">
-          {profileImage === "" ? (
+          {userInfo.image === null ? (
             ""
           ) : (
             <div onClick={(e) => handleClick(e)}>Delete</div>
