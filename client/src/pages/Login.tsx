5aff91e79132d1d7c9adb160cb0be4dcfe037b79
Daehee Yoon <allendy11@gmail.com>
2022-08-13 08:57:37 +0900

    [client] login fix css

--- client/src/pages/Login.tsx
+++ client/src/pages/Login.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import "./css/Login.css";
 import LoginModal from "../components/Login/LoginModal";
 import IUser from "../Interface/IUser";
+import Title from "../components/ETC/Title/Title";
 
 const Login = ({
   userInfo,
@@ -14,8 +15,13 @@ const Login = ({
     <div id="Login">
       <div className="container">
         <div className="box">
-          <div className="content1">Login</div>
-          <div className="content2">Welcome to TOBID.</div>
+          <Title />
+        </div>
+        <div className="box">
+          <div className="box-contents">
+            <div className="content1">Login</div>
+            <div className="content2">Welcome to TOBID.</div>
+          </div>
         </div>
         <div className="box">
           <LoginModal userInfo={userInfo} setUserInfo={setUserInfo} />
